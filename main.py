542e41387a55fc7295b04984b7cf4201ea369dcf
KENDU FRANCIS NAAHNZE <kendu@mac-3.home>
2025-04-26 16:05:07 +0200

    updated app code

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+# from flask import Flask, jsonify, request
+
+# app = Flask(__name__)
+
+
+# @app.route('/hello', methods=['GET'])
+# def helloworld():
+# 	if(request.method == 'GET'):
+# 		data = {"data": "HELLO KENDU"}
+# 		return jsonify(data)
+
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=9001)
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Healthy!", 200
 
 @app.route('/hello', methods=['GET'])
 def helloworld():
 	if(request.method == 'GET'):
 		data = {"data": "HELLO KENDU"}
 		return jsonify(data)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9001)
